4bac9d52fa44887db2e8cbc3e71558d1379ad462
jackning <270580156@qq.com>
2019-02-15 09:11:32 +0800

    update

--- xiaper-spring-boot/xiaper-spring-boot-sample-webflux/src/test/java/io/xiaper/webflux/WebfluxApplicationTests.java
+++ xiaper-spring-boot/xiaper-spring-boot-sample-webflux/src/test/java/io/xiaper/webflux/WebfluxApplicationTests.java
@@ -1,4 +1,4 @@
-package io.xiaper.starter;
+package io.xiaper.webflux;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BytedeskSpringBootSampleClientApplicationTests {
+public class WebfluxApplicationTests {
 
 	@Test
 	public void contextLoads() {
